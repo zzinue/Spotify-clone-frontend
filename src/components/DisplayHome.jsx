ec4d0baeae91cae9736d8f1c1809f51123f07319
zzinue <zzinue@gmail.com>
2024-07-22 14:33:55 -0600

    adding display home and prosp from algum items

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AllbumItem from "./AllbumItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+
+      <div className="my-5 font-bold text-2xl ">
+        {albumsData.map((item, index) => (
+          <AllbumItem
+            key={index}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+            image={item.image}
+          />
+        ))}
+      </div>
     </>
   );
 };
