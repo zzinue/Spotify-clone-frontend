29667f58197861421d184efb4d7ca0d61329b1b5
zzinue <zzinue@gmail.com>
2024-07-22 10:22:08 -0600

    player and sidebar set

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,40 @@ const Sidebar = () => {
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
       <div className="bg-[#12122] h-[15%] rounded flex flex-col justify-around">
         <div className="flex items-center gap-3 pl-8 cursor-pointer">
-          <img src={assets.home_icon} alt="" />
-          <p className="font-bold text-white">Home</p>
+          <img className="w-6" src={assets.home_icon} alt="" />
+          <p className="font-bold ">Home</p>
+        </div>
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className="w-6" src={assets.search_icon} alt="" />
+          <p className="font-bold ">Buscar</p>
+        </div>
+      </div>
+      <div className="bg-[#121212] h-[85%] rounded">
+        <div className="p-4 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img className="w-8" src={assets.stack_icon} alt="" />
+            <p className="font-semibold">Tu Biblioteca </p>
+          </div>
+          <div className="flex items-center gap-3">
+            <img className="w-5" src={assets.arrow_icon} alt="" />
+            <img className="w-5" src={assets.plus_icon} alt="" />
+          </div>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+          <h1>Crea tu primera playlist</h1>
+          <p className="font-light ">Es facil nosotros te ayudamos</p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Crear una playlist
+          </button>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
+          <h1>Buscamos algunos podcast a seguir</h1>
+          <p className="font-light ">
+            Te mantendremos actualizado sobre nuevos episodios
+          </p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Buscar podcasts
+          </button>
         </div>
       </div>
     </div>
