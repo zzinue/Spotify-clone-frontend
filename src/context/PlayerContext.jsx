50240e0694975b639ce9bbf84104c20233b759e4
zzinue <zzinue@gmail.com>
2024-07-23 08:42:19 -0600

    finished ui for spotify design

---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+  const contextValue = {};
+
+  return (
+    <PlayerContext.Provider value={contextValue}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
+};
+export default PlayerContextProvider;
