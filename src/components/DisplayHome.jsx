fdbf11f2f63c5b54174fe6b111febdf24629e399
zzinue <zzinue@gmail.com>
2024-07-22 20:48:43 -0600

    created display, display home song intem and display album

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,22 +2,42 @@ import React from "react";
 import Navbar from "./Navbar";
 import { albumsData } from "../assets/assets";
 import AllbumItem from "./AllbumItem";
+import { songsData } from "../assets/assets";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
 
-      <div className="my-5 font-bold text-2xl ">
-        {albumsData.map((item, index) => (
-          <AllbumItem
-            key={index}
-            name={item.name}
-            desc={item.desc}
-            id={item.id}
-            image={item.image}
-          />
-        ))}
+      <div className="mt-4 ">
+        <h1 className="my-5 font-bold text-2xl">Caracteristicas</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AllbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-4 ">
+        <h1 className="my-5 font-bold text-2xl">Hits del dia</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
